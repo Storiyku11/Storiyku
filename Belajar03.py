25561a4c3bc89dce22b8e5c1570e708b9aac5f2b
Storiyku11 <75144660+Storiyku11@users.noreply.github.com>
2020-11-28 06:55:49 +0700

    Create Belajar03.py

---
+++ Belajar03.py
@@ -0,0 +1,25 @@
+import os
+os.system("clear")
+def banner():
+    print("""
+    #############
+     Turorial 01
+    #############
+    ***)
+def main():
+    os.system("clear")
+    banner()
+    print("1.Hack Fecebook")
+    print("2.install Bahan")
+    print("0.Exit")
+    pilih = input("pilih Nomer:")
+    if pilih == "1":
+        os.system("git clone https://github.com/Tegar-ID/MBF")
+        os.system("cd MBF")
+        os.system("python2 MBF.py")
+    elif pilih =="2":
+        os.system("pip2 install requests")
+        os.system("pip2 install mechanize")
+    elif pilih =="0"
+
+main()
